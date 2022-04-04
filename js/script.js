46c824fe8cbba77e4437dfc4b20e005c2c82335d
Own3Dqq <own3dqq@gmail.com>
2022-04-04 15:04:18 +0300

    add func delete task, complete task

--- js/script.js
+++ js/script.js
@@ -1,23 +1,16 @@
-/* 
-	1.04.20222
-	Реализовать функцию удаления 
-	Пересмотреть и улучшить функцию добавления
-	Разобраться со localStorage и фортировкой в нем
-*/
-
 const todoList = document.querySelector('.todo__list');
-const btnAdd = document.querySelector('.btn__add');
-const inputAdd = document.querySelector('.input__add');
+const todoBtn = document.querySelector('.todo__btn');
+const inputAdd = document.querySelector('.todo__input');
 const todo__item = document.querySelector('.todo__item');
 
 const createItem = (textItem, key) => {
 	return `
-	<li class="todo__item" data-todo-state="active" data-todo-key="${key}">
-	<span class="todo__task">${textItem}</span>
-	<span class="todo__action todo__action_restore" data-todo-action="active"></span>
-	<span class="todo__action todo__action_delete" data-todo-action="deleted"></span>
-	<span class="todo__action todo__action_complete" data-todo-action="completed"></span>
-	</li>`;
+		<li class="todo__item" data-todo-state="active" data-todo-key="${key}">
+			<span class="todo__task">${textItem}</span>
+			<span class="todo__action todo__action_restore" data-todo-action="active"></span>
+			<span class="todo__action todo__action_delete" data-todo-action="deleted"></span>
+			<span class="todo__action todo__action_complete" data-todo-action="completed"></span>
+		</li>`;
 };
 
 const add = () => {
@@ -36,7 +29,7 @@ const save = () => {
 
 const use = (e) => {
 	const target = e.target;
-	if (e.keyCode == 13 || target.classList.contains('btn__add')) {
+	if (e.keyCode == 13 || target.classList.contains('todo__btn')) {
 		if (inputAdd.disabled || !inputAdd.value.length) {
 			return;
 		} else {
@@ -63,72 +56,22 @@ const init = () => {
 	}
 };
 
-const deleteItem = () => {
-	
-
-	
-	/* const action = target.dataset.todoAction;
-	const elemItem = target.closest('.todo__item');
-	if (action === 'deleted' && elemItem.dataset.todoState === 'deleted') {
-		elemItem.remove();
-	} else { */
+const removeItem = (e) => {
+	let target = e.target;
+	if (
+		target.parentNode.tagName == 'LI' &&
+		target.dataset.todoAction == 'deleted'
+	) {
+		target.parentNode.remove();
+		let itemTargetTodo = target.parentNode.dataset.todoKey;
+		localStorage.removeItem(itemTargetTodo);
+	} else if (target.dataset.todoAction == 'completed') {
+		target.parentNode.classList.toggle('complete');
+	}
 };
 
 inputAdd.addEventListener('keydown', use);
-btnAdd.addEventListener('click', use);
-init();
+todoBtn.addEventListener('click', use);
+document.addEventListener('click', removeItem);
 
-/* const todo = {
-	action(e) {
-		const target = e.target;
-		if (target.classList.contains('todo__action')) {
-			const action = target.dataset.todoAction;
-			const elemItem = target.closest('.todo__item');
-			if (action === 'deleted' && elemItem.dataset.todoState === 'deleted') {
-				elemItem.remove();
-			} else {
-				elemItem.dataset.todoState = action;
-			}
-			this.save();
-		} else if (target.classList.contains('todo__add')) {
-			this.add();
-			this.save();
-		}
-	},
-	add() {
-		const elemText = document.querySelector('.todo__text');
-		if (elemText.disabled || !elemText.value.length) {
-			return;
-		}
-		document
-			.querySelector('.todo__items')
-			.insertAdjacentHTML('beforeend', this.create(elemText.value));
-		elemText.value = '';
-	},
-	create(text) {
-		return `<li class="todo__item" data-todo-state="active">
-    <span class="todo__task">${text}</span>
-    <span class="todo__action todo__action_restore" data-todo-action="active"></span>
-    <span class="todo__action todo__action_complete" data-todo-action="completed"></span>
-    <span class="todo__action todo__action_delete" data-todo-action="deleted"></span></li>`;
-	},
-	init() {
-		const fromStorage = localStorage.getItem('todo');
-		if (fromStorage) {
-			document.querySelector('.todo__items').innerHTML = fromStorage;
-		}
-		document
-			.querySelector('.todo__options')
-			.addEventListener('change', this.update);
-		document.addEventListener('click', this.action.bind(this));
-	},
-	update() {
-		const option = document.querySelector('.todo__options').value;
-		document.querySelector('.todo__items').dataset.todoOption = option;
-		document.querySelector('.todo__text').disabled = option !== 'active';
-	},
-	save() {
-		localStorage.setItem('todo', document.querySelector('.todo__items').innerHTML);
-	},
-};
- */
+init();
