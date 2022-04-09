190edb2f02f15d43bddd9ce770e2849b47f1e6bc
Own3Dqq <own3dqq@gmail.com>
2022-04-09 12:52:55 +0300

    fixed func reload

--- js/script.js
+++ js/script.js
@@ -1,11 +1,13 @@
+/* ------------------------------------------ */
 const todoList = document.querySelector('.todo__list');
 const todoBtn = document.querySelector('.todo__btn');
 const inputAdd = document.querySelector('.todo__input');
 const todo__item = document.querySelector('.todo__item');
+const task = [];
 
-const createItem = (textItem, key) => {
+const createItem = (textItem) => {
 	return `
-		<li class="todo__item" data-todo-state="active" data-todo-key="${key}">
+		<li class="todo__item" data-todo-state="action" data-todo-key="task${task.length}">
 			<span class="todo__task">${textItem}</span>
 			<span class="todo__action todo__action_restore" data-todo-action="active"></span>
 			<span class="todo__action todo__action_delete" data-todo-action="deleted"></span>
@@ -13,18 +15,19 @@ const createItem = (textItem, key) => {
 		</li>`;
 };
 
-const add = () => {
-	todoList.insertAdjacentHTML(
-		'afterbegin',
-		createItem(inputAdd.value, inputAdd.value)
-	);
+const addTask = () => {
+	todoList.insertAdjacentHTML('afterbegin', createItem(inputAdd.value));
 	save();
+	counter();
 	inputAdd.value = '';
-	return;
 };
 
 const save = () => {
-	localStorage.setItem(inputAdd.value, inputAdd.value);
+	task.push({
+		name: `task${task.length}`,
+		text: inputAdd.value,
+		checked: 'active',
+	});
 };
 
 const use = (e) => {
@@ -32,28 +35,15 @@ const use = (e) => {
 	if (e.keyCode == 13 || target.classList.contains('todo__btn')) {
 		if (inputAdd.disabled || !inputAdd.value.length) {
 			return;
-		} else {
-			add();
 		}
-	} else if (e) {
-		return;
-	}
-};
-
-const reload = () => {
-	for (let i = 0; i < localStorage.length; i++) {
-		const key = localStorage.key(i);
-		inputAdd.value = key;
-		add();
-	}
-};
-
-const init = () => {
-	if (localStorage.length > 0) {
+		addTask();
 		reload();
-	} else {
-		return;
 	}
+};
+
+const counter = () => {
+	let count = document.querySelector('.todo__general-count');
+	count.innerHTML = localStorage.length;
 };
 
 const removeItem = (e) => {
@@ -65,9 +55,32 @@ const removeItem = (e) => {
 		target.parentNode.remove();
 		let itemTargetTodo = target.parentNode.dataset.todoKey;
 		localStorage.removeItem(itemTargetTodo);
+		counter();
 	} else if (target.dataset.todoAction == 'completed') {
 		target.parentNode.classList.toggle('complete');
 	}
+	reload();
+};
+
+const reload = () => {
+	if (todoList.children.length == 0) {
+		todoList.insertAdjacentHTML(
+			'afterbegin',
+			"<span class='message'>You have 0 task, add a new task</span>"
+		);
+	} else if (
+		todoList.children.length > 1 &&
+		todoList.querySelector('.message')
+	) {
+		todoList.querySelector('.message').remove();
+	}
+};
+
+const render = () => {};
+
+const init = () => {
+	reload();
+	/* render(); */
 };
 
 inputAdd.addEventListener('keydown', use);
