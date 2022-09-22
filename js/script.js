2e40fa7687c44ea44149248d7bfaad97441ca52f
Own3Dqq <own3dqq@gmail.com>
2022-09-22 22:28:15 +0300

    add popup window

--- js/script.js
+++ js/script.js
@@ -1,30 +1,39 @@
 /* ------------------------------------------ */
 const todoList = document.querySelector('.todo__list');
 const todoInput = document.querySelector('.form__input');
+
+let popupBg = document.querySelector('.popup__bg');
+let popup = document.querySelector('.popup');
+let popupContent = document.querySelector('.popup--content');
+let openPopupButtons = document.querySelectorAll('.button--invite');
+let closePopupButton = document.querySelector('.popup--close');
+let popupAgree = document.querySelector('.btn-agree');
+
 let tasks = [];
+/* ------------------------------------------- */
 
 const addTask = (text, date) => {
 	const task = {
 		text,
 		id: Date.now(),
 		completed: false,
-		date
+		date,
 	};
 	tasks.push(task);
 	renderAndSave(tasks);
 	todoInput.innerHTML = '';
 };
 
-const deleteTask = id => {
+const deleteTask = (id) => {
 	tasks = tasks.filter((task) => task.id !== id);
 
 	renderAndSave(tasks);
 };
 
-const completeTask = id => {
+const completeTask = (id) => {
 	tasks.map((task) => {
 		if (task.id === id) {
-			task.completed = !task.completed
+			task.completed = !task.completed;
 		}
 
 		return task;
@@ -38,15 +47,12 @@ const counterCompleted = (tasks) => {
 
 	tasks = tasks.filter((task) => task.completed === true);
 	completedTask.innerHTML = tasks.length;
-}
+};
 
 const renderTasks = (tasks) => {
 	if (!tasks.length) {
 		todoList.innerHTML = '';
-		todoList.insertAdjacentHTML(
-			'afterbegin',
-			`<span class='message'>You have 0 task, add a new task</span>`
-		);
+		todoList.insertAdjacentHTML('afterbegin', `<span class='message'>You have 0 task, add a new task</span>`);
 	} else {
 		todoList.innerHTML = '';
 		let tasksToHTML = '';
@@ -54,16 +60,35 @@ const renderTasks = (tasks) => {
 		tasks.forEach((task) => {
 			const classCompleted = task.completed ? 'list__item_completed' : '';
 			tasksToHTML += `
-				<li class="list__item item ${classCompleted}" data-todo-state="action" data-todo-key="${task.id}">
-						<span class="item__text">${task.text}</span>
-						<span class='item__date'>${task.date}</span>
-						<span class="item__action item__action_delete" data-todo-action="deleted"></span>
-						<span class="item__action item__action_complete" data-todo-action="completed"></span>
-				</li>`;
+ 				<li class="list__item item ${classCompleted}" data-todo-state="action" data-todo-key="${task.id}">
+ 						<span class="item__text">${task.text}</span>
+ 						<span class='item__date'>${task.date}</span>
+ 						<span class="item__action item__action_delete" data-todo-action="deleted"></span>
+ 						<span class="item__action item__action_complete" data-todo-action="completed"></span>
+ 				</li>`;
 		});
 		todoList.insertAdjacentHTML('afterbegin', tasksToHTML);
 	}
 };
+
+/* Form Data */
+openPopupButtons.forEach((button) => {
+	//Перебираем все кнопки
+	button.addEventListener('click', (e) => {
+		//Для каждой вешаем обработчик событий на клик
+		e.preventDefault(); //Предотвращаем дефолтное поведение браузера
+		popupBg.classList.add('active'); //Добавляем класс 'active' для фона
+		popup.classList.add('active'); //И для самого окна
+	});
+});
+
+closePopupButton.addEventListener('click', () => {
+	//Вешаем обработчик на крестик
+	popupBg.classList.remove('active'); //Убираем активный класс с фона
+	popup.classList.remove('active'); //И с окна
+});
+
+/* Popup End */
 
 const saveToLocalStorage = (tasks) => {
 	let setStorage = JSON.stringify(tasks);
@@ -103,9 +128,7 @@ const getNowDate = () => {
 		minute: '2-digit',
 		second: '2-digit',
 	};
-	return new Intl.DateTimeFormat(navigator.language, options).format(
-		new Date()
-	);
+	return new Intl.DateTimeFormat(navigator.language, options).format(new Date());
 };
 
 const renderAndSave = (tasks) => {
@@ -131,3 +154,78 @@ document.addEventListener('keydown', action);
 document.addEventListener('click', action);
 
 init();
+
+/* Form Data */
+
+// const form = document.querySelectorAll('form'),
+// 	inputs = document.querySelectorAll('input');
+
+// const message = {
+// 	loading: 'Загрузка...',
+// 	success: 'Спасибо! Скоро мы с вами свяжемся',
+// 	failure: 'Что-то пошло не так...',
+// };
+
+// const postData = async (url, data) => {
+// 	document.querySelector('.status').textContent = message.loading;
+// 	let res = await fetch(url, {
+// 		method: 'POST',
+// 		body: data,
+// 	});
+
+// 	return await res.text();
+// };
+
+// const clearInputs = () => {
+// 	inputs.forEach((item) => {
+// 		item.value = '';
+// 	});
+// };
+
+// form.forEach((item) => {
+// 	item.addEventListener('submit', (e) => {
+// 		e.preventDefault();
+
+// 		let statusMessage = document.createElement('div');
+// 		statusMessage.classList.add('status');
+// 		item.appendChild(statusMessage);
+
+// 		const formData = new FormData(item);
+
+// 		postData('https://reqres.in/api/register', formData)
+// 			.then((res) => {
+// 				console.log(res);
+// 				statusMessage.textContent = message.success;
+// 			})
+// 			.catch(() => (statusMessage.textContent = message.failure))
+// 			.finally(() => {
+// 				clearInputs();
+// 				setTimeout(() => {
+// 					statusMessage.remove();
+// 				}, 5000);
+// 			});
+// 	});
+// });
+
+const postData = async (url, data) => {
+	let res = await fetch(url, {
+		method: 'POST',
+		body: data,
+	});
+
+	return await res;
+};
+
+document.addEventListener('submit', (e) => {
+	e.preventDefault();
+	const form = document.querySelectorAll('.popup');
+	form.forEach((item) => {
+		const formData = new FormData(item);
+
+		postData('https://reqres.in/api/register', formData)
+			.then((res) => {
+				console.log(res);
+			})
+			.catch(() => console.log('2'));
+	});
+});
